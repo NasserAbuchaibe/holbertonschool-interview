1ed31dbb38c16e846f95ddf106aac52647840060
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2021-01-27 12:54:26 -0500

    fix check

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,11 +8,24 @@ sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
 def canUnlockAll(boxes):
     """ method that determines if all the boxes can be opened. """
     len_boxes = len(boxes)
-    if len_boxes == 0:
-        return True
+    # if len_boxes == 0:
+    #     return True
     for i in range(len_boxes):
         len_box = len(boxes[i])
         for x in range(1, len_box):
             if len(boxes[boxes[i][x]]) == 0 and len_boxes != i:
                 return False
     return True
+
+# def canUnlockAll(boxes):
+#     """ Write a method that determines if all the boxes can be opened
+#     """
+#     array = [0]
+#     for key in array:
+#         for key_box in boxes[key]:
+#             if key_box not in array:
+#                 if key_box < len(boxes):
+#                     array.append(key_box)
+#     if len(array) == len(boxes):
+#         return True
+#     return False
